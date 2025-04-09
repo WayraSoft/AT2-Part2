7bb74a2fba188f7958f28112046fe385d499182d
Wayra <wayrasoft10@gmail.com>
2025-04-09 10:29:59 +1000

    Part2-Q2.1
    New object created with the properties required.

---
+++ moviePart2.js
@@ -0,0 +1,13 @@
+// File: moviePart2.js
+// This file shows an object and its properties and methods.
+// Author: Carlos
+// Date: 01/04/2025
+
+// Q2.1 This Object store movies and contains two properties, the name
+//      of the movie and the release year.
+const myMovie = {
+  title: 'The longest day',
+  year: 1964
+};
+
+console.log(myMovie);
